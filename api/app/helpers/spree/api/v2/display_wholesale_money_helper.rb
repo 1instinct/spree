58610e4c9c845146491d8ecf98c1b64204341f24
David Dombrowsky <davek@6thstreetradio.org>
2021-08-06 13:28:55 -0400

    added wholesale product serializers

---
+++ api/app/helpers/spree/api/v2/display_wholesale_money_helper.rb
@@ -0,0 +1,47 @@
+module Spree
+  module Api
+    module V2
+      # This is identical to DisplayMoneyHelper except
+      # for which price type is selected (amount or wholesale_price).
+      module DisplayWholesaleMoneyHelper
+        extend ActiveSupport::Concern
+
+        class_methods do
+          def find_price(product_or_variant, currency)
+            product_or_variant.price_in(currency)
+          end
+
+          def price(product_or_variant, currency)
+p 'WHOLESALE'
+            price = find_price(product_or_variant, currency)
+p price
+            format('%.2f', price.wholesale_price)
+          end
+
+          def display_price(product_or_variant, currency)
+p 'WHOLESALE'
+            price = find_price(product_or_variant, currency)
+p price
+            return nil if price.new_record?
+
+            Spree::Money.new(price.wholesale_price, currency: currency).to_s
+          end
+
+          def compare_at_price(product_or_variant, currency)
+            price = find_price(product_or_variant, currency)
+            return nil if price.new_record? || price.compare_at_amount.blank?
+
+            format('%.2f', price.compare_at_amount)
+          end
+
+          def display_compare_at_price(product_or_variant, currency)
+            price = find_price(product_or_variant, currency)
+            return nil if price.new_record? || price.compare_at_amount.blank?
+
+            Spree::Money.new(price.compare_at_amount, currency: currency).to_s
+          end
+        end
+      end
+    end
+  end
+end
